c79e7636e2fe8f57b4f9167b4cd3e2af0e691304
bijubayarea <bijubayarea@gmail.com>
2022-11-05 14:04:48 -0700

    cleanup file

--- terraform-aws-route53/locals.tf
+++ terraform-aws-route53/locals.tf
@@ -14,22 +14,3 @@ locals  {
 locals  {
   iam_role          = aws_iam_role.role.arn
 }
-
-
-/*varaiabl "issuer_arn" {
-  description = "EKS cluster OIDC ARN"
-  type        = string
-  default     = data.aws_iam_openid_connect_provider.eks_oidc_provider.arn
-}
-
-variable "issuer_url" {
-  description = "EKS cluster OIDC ARN"
-  type        = string
-  default     = replace(data.aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")
-}*/
-
-#variable "iam_role" {
-#  description = "iam role"
-#  type        = string
-#  default     = aws_iam_role.role[0].arn
-#}
